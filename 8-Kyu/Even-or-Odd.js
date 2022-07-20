6ebb13aac735f6fe44e094241e683594835a9147
Oscar Cortez <oacortez98@gmail.com>
2022-07-19 20:43:50 -0600

    Add psuedocode

--- 8-Kyu/Even-or-Odd.js
+++ 8-Kyu/Even-or-Odd.js
@@ -6,3 +6,23 @@ Create a function that takes an integer as an argument and returns
 function even_or_odd(number) {
   
 }
+
+
+// PROBLEM: RETURN A STRING OF "EVEN" OR ODD BASED ON THE NUMBER INPUTED AS AN ARGUMENT
+// INPUT: A NUMBER 
+// OUTPUT: STRING 
+
+// PLAN:
+// CREATE A CONDITIONAL (IF ELSE) -- 
+// CALCULATE -- HOW TO DETERMINE IF THE INPUT NUMBER IS EVEN OR ADD
+// WE ARE GOING TO USE SOME SORT OF MATH
+
+// GOOGLED: "how to calculate a odd or even"
+// If a number is evenly divisible by 2 with no remainder, then it is even. 
+// You can calculate the remainder with the modulo operator % like this num % 2 == 0. 
+// If a number divided by 2 leaves a remainder of 1, then the number is odd
+
+
+// note: 
+// * Did not think of edge case ex; -0 or -1 
+// * I covered it in my conditional
