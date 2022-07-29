7e169bc1fecad76c03ead7bc44e155ee752af9b8
Oscar Cortez <oacortez98@gmail.com>
2022-07-28 18:27:48 -0600

    Add brainstorm notes

--- 7-Kyu/Disemvowel-Trolls.js
+++ 7-Kyu/Disemvowel-Trolls.js
@@ -11,8 +11,35 @@ would become -> "Ths wbst s fr lsrs LL!".
 Note: for this kata y isn't considered a vowel.
 */
 
+// Solution: 
+
 
 // ====================================================================
 
 
-/* */
+/* 
+PSUEDOCODE ⬇️
+
+PROBLEM: CREATE A FUNCTION THAT TAKES A PARAMETER - STRING. THIS FUNCTION
+NEEDS TO REMOVE ALL VOWELS - A,E,I,O,U BUT NOT Y. RETURN A NEW STRING 
+WITH ALL THE VOWELS REMOVED. 
+
+INPUT: STRING
+OUTPUT: NEW STRING WITH VOWELS REMOVED
+
+BRAINSTORM: 
+- I might need to create a variable and start it off as a empty string
+- create an array and assign them to vowels
+- I need to iterate every single letter from the parameter 
+- check the array of vowels with the parameter
+- create a conditional checking if it contains any vowels
+- remove every single vowel expect for "y" 
+- return a new string 
+
+- edge cases: 
+- The parameter has upper and lower case string values
+- need to convert all strings to lowercase to have no issues
+
+
+
+*/
