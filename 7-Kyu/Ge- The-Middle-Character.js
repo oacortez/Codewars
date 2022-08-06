0b76fc0cb7a32dbabed21a8a81d23177ca52e986
Oscar Cortez <oacortez98@gmail.com>
2022-08-05 20:24:27 -0600

    Add psuedocode and brainstorm

--- 7-Kyu/Ge- The-Middle-Character.js
+++ 7-Kyu/Ge- The-Middle-Character.js
@@ -23,3 +23,29 @@ that you do not need to worry about your solution timing out.
 The middle character(s) of the word represented as a string.
 
  */
+
+
+/**
+ 
+/*
+Problem: 
+I am given an string input. I need to return the middle of the strin value.
+If the word lenght is odd return the middle character. If the length is even return the 
+2 middle letters in that string as a string.
+
+Input: String
+
+Output: Middle letter if its ODD or the 2 middle character if its an EVEN length - String value
+
+
+Brainstorm:
+- Create an empty array to push my solution 
+- Check if its odd or even 
+- divide the lenght of the value using a module (lenght % 2 )
+- check if the the number is odd or even (num % 2 === 0)
+- based on that figure out how to get the middle character or the 2 characters in that string
+- Return it as a string
+
+*/
+
+ */
