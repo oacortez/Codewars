aee29fc5ca438c403dbcd90195842fc282badb34
Oscar Cortez <oacortez98@gmail.com>
2022-08-17 20:25:44 -0600

    Add pseudo

--- 8-Kyu/remove-first-and-last.js
+++ 8-Kyu/remove-first-and-last.js
@@ -6,3 +6,21 @@
   You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
  */
+
+
+  /**
+   Psuedo:
+
+   Problem: I am given a string and need to remove the first and last index num from that string 
+
+   input: string
+   output: new string with first and last str removed
+
+  brainstorm:
+  - I need to convert string to an array 
+  - use .split('') to make each individual character a string index
+  - remove first and last character using .shift() - (removes first index value) + .pop() - (removes last index value)
+  - next convert your array to become a string again 
+  - return  .join('') - to convert as a string
+
+   */
