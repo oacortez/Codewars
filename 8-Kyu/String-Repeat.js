c957876e5a1d76678f65d92ed4b4c8b8b74b8887
Oscar Cortez <oacortez98@gmail.com>
2022-10-05 14:43:35 -0600

    Add brainstrom/psuedo

--- 8-Kyu/String-Repeat.js
+++ 8-Kyu/String-Repeat.js
@@ -9,3 +9,16 @@ Examples (input -> output)
 
 */
 
+
+
+
+/**
+Problem: I am given two inputs; a number and a string. and I need to return the a string of 
+the amount that is going to be repeated
+
+input: number & String 
+output: repeated string amount based on the number input given
+
+- brainstrom: 
+  * Need to check for any methods I can user with string - Maybe - .repeat() ?
+*/ 
