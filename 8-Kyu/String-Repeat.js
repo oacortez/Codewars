0aacdd61ffe5cefcd8175e9603bf76a8107a4c82
Oscar Cortez <oacortez98@gmail.com>
2022-10-05 14:44:44 -0600

    Add final thoughts/conclusion

--- 8-Kyu/String-Repeat.js
+++ 8-Kyu/String-Repeat.js
@@ -26,3 +26,17 @@ output: repeated string amount based on the number input given
 - brainstrom: 
   * Need to check for any methods I can user with string - Maybe - .repeat() ?
 */ 
+
+/** 
+Conclusion:
+
+I overthought this one for a bit since its my first time coding in JS again. But I was
+able to pick it up fast by looking at the documents. I found out about 
+.repeat() and how is able to take a string and able to repeat it as many times as you would like.
+Some of the edge cases I was thinking were;
+
+- Space takes count 
+- if numbers would be hold count 
+
+Overall it was very easy and a good intro.
+*/
