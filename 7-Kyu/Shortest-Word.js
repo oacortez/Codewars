d371caadda5aea2c904eaf05cc3c81525a4d9e52
Oscar Cortez <oacortez98@gmail.com>
2022-10-06 14:01:10 -0600

    Add brainstorm/psuedo'

--- 7-Kyu/Shortest-Word.js
+++ 7-Kyu/Shortest-Word.js
@@ -3,7 +3,16 @@
  Simple, given a string of words, return the length of the shortest 
  word(s). String will never be empty and you do not need to account for 
  different data types.
- 
+
  */
 
 
+ 
+/** 
+PROBLEM: I am given a string of words, I need to find the shortest word and return the
+length amount of that shortest word 
+
+input: string of words
+output: Number - the shortest word lenght found
+
+*/
