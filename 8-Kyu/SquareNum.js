a8482076fc50bcaa6f3d7dd8f16b2f91ce692fde
Oscar Cortez <oacortez98@gmail.com>
2022-10-09 17:15:35 -0600

    Add prompt

---
+++ 8-Kyu/SquareNum.js
@@ -0,0 +1,7 @@
+/** 
+PROMPT: Complete the square sum function so that it squares each number passed into it and 
+then sums the results together.
+
+For example: [1, 2, 2] 
+it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+ */
