dc0c5cd1b34b79e7ba8b4ffa4bad3995ee909a2a
Oscar Cortez <oacortez98@gmail.com>
2023-08-23 19:04:58 -0600

    Add psudocode

--- 8-Kyu/Double-char.js
+++ 8-Kyu/Double-char.js
@@ -10,5 +10,27 @@ Examples (Input -> Output):
  */
 
 function doubleChar(str) {
-  // Your code here
+let newStr = []
+  
+for (let i = 0; i < str.length; i++) {
+  newStr.push(str[i].repeat(2))
 }
+    return newStr.join('')
+}
+
+/**\
+ 
+PsuedoCode:
+
+// - I am given a string 
+// - I need to return each charather of the string twice 
+// - example: hi -> hhii
+
+- assign an empty array 
+- loop over character 
+- using the string method .repeat() - grab each index and attach to it 
+  - str[index].repeat(2) - Repeat each index(character) twice (2)
+- push each character into the empty array
+- using .join() - to join each index together as a string
+- return value for full result
+ */
