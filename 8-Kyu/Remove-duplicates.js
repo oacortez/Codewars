983bf8d60a8bf7d865979764fae9ea0a9b192f27
Oscar Cortez <oacortez98@gmail.com>
2023-08-30 19:40:32 -0600

    Add final comments for solution

--- 8-Kyu/Remove-duplicates.js
+++ 8-Kyu/Remove-duplicates.js
@@ -33,5 +33,11 @@ PSEUDOCODE:
   back to an array
   
   - I also used the spread operator for the new array
-  - 
+  - The spread (...) syntax allows an iterable, such as an array or string, 
+  to be expanded in places where zero or more arguments (for function calls) 
+  or elements (for array literals) are expected. In an object literal, 
+  the spread syntax enumerates the properties of an object and 
+  adds the key-value pairs to the object being created
+  
+  
  */
