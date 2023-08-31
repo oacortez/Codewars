cb13e2c26a68d1111271072e94845cb07dbafa3c
Oscar Cortez <oacortez98@gmail.com>
2023-08-30 19:23:47 -0600

    Add starter function

--- 8-Kyu/Remove-duplicates.js
+++ 8-Kyu/Remove-duplicates.js
@@ -5,9 +5,12 @@ Define a function that removes duplicates from an array of non negative numbers 
 The order of the sequence has to stay the same.
 
 Examples:
-
 Input -> Output
 [1, 1, 2] -> [1, 2]
 [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
 
  */
+
+
+function distinct(a) {
+}
