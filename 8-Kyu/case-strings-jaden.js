14bb1f0466050fed08cbbd121081697814d10b79
Oscar Cortez <oacortez98@gmail.com>
2023-09-05 18:48:03 -0600

    Add starter function

--- 8-Kyu/case-strings-jaden.js
+++ 8-Kyu/case-strings-jaden.js
@@ -14,3 +14,6 @@ Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
  */
+
+String.prototype.toJadenCase = function () {
+};
