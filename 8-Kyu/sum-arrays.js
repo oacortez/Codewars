3e6cc4730cba6f3c0eeb12b4f581cadf9b6f2d7a
Oscar Cortez <oacortez98@gmail.com>
2023-09-06 22:45:07 -0600

    Add solution

--- 8-Kyu/sum-arrays.js
+++ 8-Kyu/sum-arrays.js
@@ -22,5 +22,14 @@ You can assume that you do get an array and if the array is empty, return 0.
  */
 
 function sum (numbers) {
-  "use strict";
+let result = 0; 
+
+if(numbers.length === 0) {
+  return result
+} else {
+  for(let i = 0; i < numbers.length; i++) {
+    result += numbers[i]
+  }
+}
+  return result
 };
