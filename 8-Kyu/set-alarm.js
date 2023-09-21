2bf65455c3aa5a89e0ae64bcdfb350ce64a83098
Oscar Cortez <oacortez98@gmail.com>
2023-09-20 21:04:18 -0600

    Add solution

--- 8-Kyu/set-alarm.js
+++ 8-Kyu/set-alarm.js
@@ -20,4 +20,11 @@ false    | false    => false
 
 
 function setAlarm(employed, vacation){
+  if(employed === true && vacation === true) {
+    return false
+  } else if (employed === true && vacation === false) {
+    return true
+  } else if (employed === false && vacation === true) {
+    return false
+  }
 }
