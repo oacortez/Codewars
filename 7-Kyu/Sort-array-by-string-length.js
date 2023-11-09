a4e4512383375f597f47156b8b2c686c59906fac
Oscar Cortez <oacortez98@gmail.com>
2023-11-08 22:07:46 -0700

    Add Starter function

--- 7-Kyu/Sort-array-by-string-length.js
+++ 7-Kyu/Sort-array-by-string-length.js
@@ -15,3 +15,6 @@ Your function would return the following array:
 All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
 
  */
+
+function sortByLength (array) {
+}
