e6374de1220282e1f7a0388045aaee1950740e29
Oscar Cortez <oacortez98@gmail.com>
2023-11-11 19:46:57 -0700

    Add steps to solve problem

--- 8-Kyu/python-codewars/even-or-odd.py
+++ 8-Kyu/python-codewars/even-or-odd.py
@@ -14,4 +14,10 @@ Output: String of Even or Odd based on return result
 
 General thought: I am given a number type and based off that I need to create math solution to find Even or Odd
 
+Steps:
+- Use conditional if statement in other to check if Number is odd or even
+- Use module math (%) this checks if number is 0 or 1 based on number 
+- return string of even or odd based on return result
+- should pass test!
+
 """
