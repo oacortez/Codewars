f58473c6c40c0d01050171d097880a55ea64e18d
Oscar Cortez <oacortez98@gmail.com>
2023-11-11 19:42:03 -0700

    General thought

--- 8-Kyu/python-codewars/even-or-odd.py
+++ 8-Kyu/python-codewars/even-or-odd.py
@@ -12,6 +12,6 @@ def even_or_odd(number):
 Input: Number
 Output: String of Even or Odd based on return result
 
-
+General thought: I am given a number type and based off that I need to create math solution to find Even or Odd
 
 """
