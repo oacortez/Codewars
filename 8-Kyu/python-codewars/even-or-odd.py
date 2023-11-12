2d5db712c324c45cc715b7e94220461369b5287c
Oscar Cortez <oacortez98@gmail.com>
2023-11-11 19:04:30 -0700

    Add input and output

--- 8-Kyu/python-codewars/even-or-odd.py
+++ 8-Kyu/python-codewars/even-or-odd.py
@@ -6,3 +6,12 @@ PROMPT: Create a function that takes an integer as an argument and returns "Even
 
 def even_or_odd(number):
   pass
+
+
+"""
+Input: Number
+Output: String of Even or Odd based on return result
+
+
+
+"""
