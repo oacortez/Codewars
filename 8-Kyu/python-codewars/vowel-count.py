f3e693c9fd28ce0d2732d6116829b2db16fd4a61
Oscar Cortez <oacortez98@gmail.com>
2023-12-21 19:56:56 -0700

    Starter function

--- 8-Kyu/python-codewars/vowel-count.py
+++ 8-Kyu/python-codewars/vowel-count.py
@@ -9,3 +9,6 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.
 
 """
+
+def get_count(sentence):
+    pass
