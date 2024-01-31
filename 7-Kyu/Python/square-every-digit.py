e39327d96821ca01a9d75aa84d3f15fc50df9932
Oscar Cortez <oacortez98@gmail.com>
2024-01-30 17:28:36 -0700

    Include input and output

--- 7-Kyu/Python/square-every-digit.py
+++ 7-Kyu/Python/square-every-digit.py
@@ -2,7 +2,7 @@
 
 In this kata, you are asked to square every digit of a number and concatenate them.
 
-For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1. (81-1-1-81)
 
 Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
 
@@ -12,3 +12,12 @@ Note: The function accepts an integer and returns an integer.
 
 def square_digits(num):
     # Your code here
+  
+
+
+  '''
+    Input: Number 
+    Ouput: Number
+    
+
+  '''
