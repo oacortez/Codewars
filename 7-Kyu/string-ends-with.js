87ac135fe8ca7e1f78f6e74660cdff34fa38c386
Oscar Cortez <oacortez98@gmail.com>
2024-01-31 19:20:39 -0700

    Add steps to solve problem

--- 7-Kyu/string-ends-with.js
+++ 7-Kyu/string-ends-with.js
@@ -18,4 +18,13 @@ function solution(str, ending){
 Input: String
 Output: true/false statement
 
+Steps:
+- I am given a function with 2 parameters(both params are strings)
+- Since there are both strings I need to remove the first parameter to match the second parameter 
+- Create a variable the holds only the length of the second parameter using .length
+- Create another variable now that removes the first parameter to match the second parameter using .substr()
+= If statement to check if the first param matches the second param
+  - if yes return true 
+  - else return false 
+
  */
