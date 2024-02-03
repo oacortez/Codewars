6c095fa9dd86820e4884dade781caa9270707ab9
Oscar Cortez <oacortez98@gmail.com>
2024-02-02 18:28:59 -0700

    Add steps to solve problem

--- 7-Kyu/remove-anchor-url.js
+++ 7-Kyu/remove-anchor-url.js
@@ -17,4 +17,10 @@ function removeUrlAnchor(url){
 Input: String
 Output: String
 
+Steps:
+- Create a variable to store the split string from the parameter 
+- Use .split() in the string and store the anchor character - url.split("#")
+- Next create another variable to modifie the url and only get everything before the "#"
+- return the modified url that is stored in the variable created
+
  */
