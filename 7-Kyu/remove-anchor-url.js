a8ff905ac03a861b690457e749188ea0e8a02ea4
Oscar Cortez <oacortez98@gmail.com>
2024-02-02 18:37:56 -0700

    Create a note about not being fully dynamic code

--- 7-Kyu/remove-anchor-url.js
+++ 7-Kyu/remove-anchor-url.js
@@ -20,7 +20,10 @@ Output: String
 Steps:
 - Create a variable to store the split string from the parameter 
 - Use .split() in the string and store the anchor character - url.split("#")
-- Next create another variable to modifie the url and only get everything before the "#"
+- Next create another variable to modified the url and only get everything before the "#"
 - return the modified url that is stored in the variable created
+
+Note: This code is not fully dynamic due to if we changed a test case to 'www.codewars.com/katas/#about?page=1' the code would then return
+"www.codewars.com/katas/" instead of 'www.codewars.com/katas/?page=1'
 
  */
