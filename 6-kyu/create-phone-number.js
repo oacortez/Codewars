ac62aaeab8eae96fce2a52efb892332f1b72abc7
Oscar Cortez <oacortez98@gmail.com>
2024-02-06 17:02:02 -0700

    Final code solution in function

--- 6-kyu/create-phone-number.js
+++ 6-kyu/create-phone-number.js
@@ -11,6 +11,13 @@ Don't forget the space after the closing parentheses!
  */
 
 function createPhoneNumber(numbers){
+
+  let areaCode = numbers.slice(0, 3).join('');
+  let firstPart = numbers.slice(3, 6).join('');
+  let secondPart = numbers.slice(6, 10).join('');
+  
+  return `(${areaCode}) ${firstPart}-${secondPart}`;
+
 }
 
 /**
