41364483b8db69102bbbaba19fd94c5467b693e1
Oscar Cortez <oacortez98@gmail.com>
2024-07-15 20:45:20 -0600

    Add prompt

---
+++ 7-Kyu/reverse-letters-#2.js
@@ -0,0 +1,10 @@
+// Task:
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+
+
