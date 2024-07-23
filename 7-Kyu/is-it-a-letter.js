480ca70cedeabcdfc4fa26f5e8ae46393c216434
Oscar Cortez <oacortez98@gmail.com>
2024-07-22 21:31:17 -0600

    Add starter function

--- 7-Kyu/is-it-a-letter.js
+++ 7-Kyu/is-it-a-letter.js
@@ -4,3 +4,7 @@ Write a function, isItLetter or is_it_letter or IsItLetter,
 which tells us if a given character is a letter or not.
 
  */
+
+function isItLetter(character) {
+  return true || false ;
+}
