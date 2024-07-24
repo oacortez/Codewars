75ccd9c1f577566223b612b08ccd531e397cb098
Oscar Cortez <oacortez98@gmail.com>
2024-07-23 20:47:40 -0600

    Add final psuedo steps to solve problem

--- 7-Kyu/is-it-a-letter.js
+++ 7-Kyu/is-it-a-letter.js
@@ -15,6 +15,13 @@ Input: String
 Output: True or False
 
 
-
-
+- I had to google this solution to find out helper functions because I was not 
+able to figure it out
+- I solve this solution using for of loop.
+- first we create the for of loop
+- inside the for loop we create a if statement
+- in the if state we check if a character is not more than or equal to lowercase 'a
+and character is less than or equal to lowercase 'z'
+- and we also check if a character is less or more than or equal to uppercase A and Z 
+then return false else return true.
  */
