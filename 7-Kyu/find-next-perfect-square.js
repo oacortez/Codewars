fa9e95aef608483bf83a5fe588ed6334120ac667
Oscar Cortez <oacortez98@gmail.com>
2024-07-30 22:33:42 -0600

    consolo.logging variables

--- 7-Kyu/find-next-perfect-square.js
+++ 7-Kyu/find-next-perfect-square.js
@@ -16,4 +16,8 @@ function findNextSquare(sq) {
   if(perfect % 1 !== 0) {
     return -1
   }
+  const nextSqrt = sq + 1;
+  console.log(nextSqrt)
+//    console.log(nextSqrt)
+  console.log(nextSqrt * nextSqrt);
 }
